a7d68c5681b5deb67219f559bedfcdd8ce9438d3
lucy <lucy.shepherd@cnous.fr>
2021-03-27 10:37:48 +0100

    bootstrap version change + recipe component added

--- shop4recipes/src/app/components/recipes/recipes-list/recipes-list.component.ts
+++ shop4recipes/src/app/components/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'description', 'https://vegan.rocks/blog/vegan-cheese/feature_hud861e682c4abc7b13b5a4e3849d7046f_973666_1600x900_fill_q75_box_smart1.jpg')
+  ];
 
   constructor() { }
 
